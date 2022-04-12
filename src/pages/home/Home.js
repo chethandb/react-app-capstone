c6f9bc6341e741c2286963719e3082f3ea6bebe8
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-04-12 00:08:40 -0500

    added condition to redirect to list or add details

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,21 @@
-import { useAuthContext } from '../../hooks/useAuthContext'
-
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 // styles
-import styles from './Home.module.css'
+import styles from './Home.module.css';
 
 // components
-import StudentForm from './StudentForm'
-
+import StudentForm from './StudentForm';
+import StudentList from './StudentList';
 
 export default function Home() {
-  const { user } = useAuthContext()
-  
- /* return (
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection(
+    'students',
+    ['uid', '==', user.uid],
+    ['createdAt', 'desc']
+  );
+  /* return (
     <div className={styles.container}>
       
       <div className={styles.sidebar}>
@@ -21,13 +25,19 @@ export default function Home() {
   )
 }*/
 
-return (
-  <div className={styles.container}>
-    
-    <div className={styles.sidebar}>
-   
-      <StudentForm uid={user.uid}/>
+  return (
+    <div className={styles.container}>
+      {error && <p>{error}</p>}
+      {!documents && (
+        <div className={styles.sidebar}>
+          <StudentForm uid={user.uid} />
+        </div>
+      )}
+      {documents && (
+        <div className={styles.content}>
+          <StudentList students={documents} />
+        </div>
+      )}
     </div>
-  </div>
-)
+  );
 }
--- src/pages/home/StudentForm.js
+++ src/pages/home/StudentForm.js
@@ -1,49 +1,47 @@
-import { useState, useEffect } from 'react'
-import { useFirestore } from '../../hooks/useFirestore'
-import { NavLink, useHistory   } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { useFirestore } from '../../hooks/useFirestore';
+import { NavLink, useHistory } from 'react-router-dom';
 
-import ListDisplay from './ListDisplay'
+import ListDisplay from './ListDisplay';
 
-export default function StudentForm( { uid } ) {
-  const [name, setName] = useState('')
-  const [nuid, setNuid] = useState('')
-  const [course, setCourse] = useState('')
-  const [creditscomplete, setCreditscomplete] = useState('')
-  const [lastsem, setLastsem] = useState('')
-  const [optflag, setOptflag] = useState('')
-  const [optstartdate, setOptstartdate] = useState('')
-  const { addDocument, response } = useFirestore('students')
+export default function StudentForm({ uid }) {
+  const [name, setName] = useState('');
+  const [nuid, setNuid] = useState('');
+  const [course, setCourse] = useState('');
+  const [creditscomplete, setCreditscomplete] = useState('');
+  const [lastsem, setLastsem] = useState('');
+  const [optflag, setOptflag] = useState('');
+  const [optstartdate, setOptstartdate] = useState('');
+  const { addDocument, response } = useFirestore('students');
   let history = useHistory();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     addDocument({
       uid,
-      name, 
+      name,
       nuid,
       course,
       creditscomplete,
       lastsem,
       optflag,
       optstartdate,
-    })
+    });
     history.push('/listdisplay');
-  }
+  };
 
-  
-      // reset the form fields
+  // reset the form fields
   useEffect(() => {
     if (response.success) {
-      setName('')
-      setNuid('')
-      setCourse('')
-      setCreditscomplete('')
-      setLastsem('')
-      setOptflag('')
-      setOptstartdate('')
-      
+      setName('');
+      setNuid('');
+      setCourse('');
+      setCreditscomplete('');
+      setLastsem('');
+      setOptflag('');
+      setOptstartdate('');
     }
-  }, [response.success])
+  }, [response.success]);
 
   return (
     <>
@@ -51,11 +49,11 @@ export default function StudentForm( { uid } ) {
       <form onSubmit={handleSubmit}>
         <label>
           <span>Name:</span>
-          <input 
+          <input
             type="text"
             required
-            onChange={(e) => setName(e.target.value)} 
-            value={name} 
+            onChange={(e) => setName(e.target.value)}
+            value={name}
           />
         </label>
         <label>
@@ -63,8 +61,8 @@ export default function StudentForm( { uid } ) {
           <input
             type="number"
             required
-            onChange={(e) => setNuid(e.target.value)} 
-            value={nuid} 
+            onChange={(e) => setNuid(e.target.value)}
+            value={nuid}
           />
         </label>
         <label>
@@ -72,8 +70,8 @@ export default function StudentForm( { uid } ) {
           <input
             type="text"
             required
-            onChange={(e) => setCourse(e.target.value)} 
-            value={course} 
+            onChange={(e) => setCourse(e.target.value)}
+            value={course}
           />
         </label>
         <label>
@@ -81,8 +79,8 @@ export default function StudentForm( { uid } ) {
           <input
             type="number"
             required
-            onChange={(e) => setCreditscomplete(e.target.value)} 
-            value={creditscomplete} 
+            onChange={(e) => setCreditscomplete(e.target.value)}
+            value={creditscomplete}
           />
         </label>
         <label>
@@ -90,8 +88,8 @@ export default function StudentForm( { uid } ) {
           <input
             type="boolean"
             required
-            onChange={(e) => setLastsem(e.target.value)} 
-            value={lastsem} 
+            onChange={(e) => setLastsem(e.target.value)}
+            value={lastsem}
           />
         </label>
         <label>
@@ -99,8 +97,8 @@ export default function StudentForm( { uid } ) {
           <input
             type="boolean"
             required
-            onChange={(e) => setOptflag(e.target.value)} 
-            value={optflag} 
+            onChange={(e) => setOptflag(e.target.value)}
+            value={optflag}
           />
         </label>
         <label>
@@ -108,14 +106,12 @@ export default function StudentForm( { uid } ) {
           <input
             type="date"
             required
-            onChange={(e) => setOptstartdate(e.target.value)} 
-            value={optstartdate} 
+            onChange={(e) => setOptstartdate(e.target.value)}
+            value={optstartdate}
           />
         </label>
         <button> Add Details</button>
-        
-        
       </form>
     </>
-  )
+  );
 }
