26d0a63474501588457d1d1e4f4bf8ac51bbcbb2
Chethan D. Balakrishna <iamchethandb@gmail.com>
2022-04-17 16:05:25 -0500

    changes for home page

--- src/pages/home/ListDisplay.js
+++ src/pages/home/ListDisplay.js
@@ -1,24 +1,26 @@
-import { useAuthContext } from '../../hooks/useAuthContext'
-import { useCollection } from '../../hooks/useCollection'
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 // styles
-import styles from './Home.module.css'
+import styles from './Home.module.css';
 
 // components
-import StudentList from './StudentList'
+import StudentList from './StudentList';
 
 export default function ListDisplay() {
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
   const { documents, error } = useCollection(
-    'students', ["uid", "==", user.uid], ['createdAt', 'desc']
-  )
+    'students',
+    ['uid', '==', user.uid],
+    ['createdAt', 'desc']
+  );
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-      {error && <p>{error}</p>}
+        {error && <p>{error}</p>}
         {documents && <StudentList students={documents} />}
-              </div>
-         </div>
-  )
+      </div>
+    </div>
+  );
 }
